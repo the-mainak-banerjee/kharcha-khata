a2c7b811a4374a4af858359823a5554f9b140a46
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-03-16 20:32:31 +0530

    feat: show item on dom

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let expenseForm = document.querySelector("#expenseForm")
 let expenseDesc = document.querySelector("#expenseDesc")
 let expenseAmount = document.querySelector("#expenseAmount")
 let addExpense = document.querySelector("#addExpense")
+let showExpense = document.querySelector("#showExpense")
 
 let expenseDetailsArr = []
 
+showItem();
 expenseForm.addEventListener("submit",submitForm)
 
 function storeLocally() {
@@ -13,7 +15,6 @@ function storeLocally() {
         amount: `${expenseAmount.value}`
     }
     let expenseItem = localStorage.getItem("expense")
-    let expenseObj = [];
 
     if(expenseItem === null){
         expenseObj = []
@@ -25,8 +26,30 @@ function storeLocally() {
     localStorage.setItem("expense", JSON.stringify(expenseObj))
 }
 
+function showItem() {
+    let expenseItem = localStorage.getItem("expense")
+
+    if(expenseItem === null){
+        expenseObj =[]
+    }
+    else{
+        expenseObj = JSON.parse(expenseItem)
+    }
+    let allExpenseItem = expenseObj.map((element,index) => {
+        return ` <tr>
+        <td>${index+1}</td>
+        <td>${element.topic}</td>
+        <td>${element.amount}</td>
+        <td>Edit</td>
+        <td>Delete</td>
+    </tr>`
+    })
+    showExpense.innerHTML = allExpenseItem.join("")
+    
+}
+
 function submitForm(evt){
     evt.preventDefault();
-    console.log(expenseDesc.value)
-    storeLocally()
+    storeLocally();
+    showItem();
 }
